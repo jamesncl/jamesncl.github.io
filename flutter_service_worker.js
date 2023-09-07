21f847976338547071d1c7bd01ddafc4bb5c1f96
jamesncl <jamesncl@users.noreply.github.com>
2023-09-07 16:20:33 +0100

    Added visual timeline

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -52,9 +52,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "eee17a34ddaf338ac63814834cd8a6be
 "icons/Icon-maskable-192.png": "abc23704bb99b980c355053e2b36b4e8",
 "icons/Icon-maskable-512.png": "9a8d34eed9a34e253752a85296b4490b",
 "icons/loading.png": "0afc232eda2bbcc02471a406ce9a4f41",
-"index.html": "a51e2f4f62a5839be5ff650f77926cae",
-"/": "a51e2f4f62a5839be5ff650f77926cae",
-"main.dart.js": "daa0535dffb3ccd3abb1e259ff7263b3",
+"index.html": "d03a487d5230a07a8e6292aa1d945113",
+"/": "d03a487d5230a07a8e6292aa1d945113",
+"main.dart.js": "ddde066b4c001f56f360f220aaa80428",
 "manifest.json": "ff4493ca5729822f04868032a3ce42c4",
 "rive/headlines.riv": "36716ea1497aacc28f52824f151cfdd5",
 "version.json": "a26005e92a6381740a8b1e7adac04f5a"};
